d17d4347ea9f73e3307a67e8a578c751424af97b
magacode <48868211+magacode@users.noreply.github.com>
2020-09-17 12:00:41 +0300

    Counter update

--- src/counter.js
+++ src/counter.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
 
 const Counter = ({ counter, dec, inc, rnd }) => {
   return (
     <div className="wrap">
       <div className="counter">{counter}</div>
-      <button className="btn" onClick={inc}>INC</button>
-      <button className="btn" onClick={dec}>DEC</button>
-      <button className="btn" onClick={rnd}>RND</button>
+      <button className="btn" onClick={inc}>
+        INC
+      </button>
+      <button className="btn" onClick={dec}>
+        DEC
+      </button>
+      <button className="btn" onClick={rnd}>
+        RND
+      </button>
     </div>
   );
-}
+};
 
-export default Counter;
+export default connect()(Counter);
